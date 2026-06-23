4a5fa860c926f7be461dc77cd5b129422cb170ca
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-23 20:00:38 +0330

    implement tasks admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Board, Column, Task
 
-# Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project', 'is_default', 'datetime_created')
+
+
+@admin.register(Column)
+class ColumnAdmin(admin.ModelAdmin):
+    list_display = ('name', 'board', 'position')
+    list_editable = ('position',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'column', 'assignee', 'priority', 'due_date')
+    list_filter = ('priority', 'column')
+    search_fields = ('title', 'description')
+    raw_id_fields = ('project', 'column', 'assignee')
