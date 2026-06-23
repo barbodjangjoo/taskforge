6aba1bf16c334bf11ed1bbb86e39c9a259e22a5d
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-23 13:23:44 +0330

    implement board model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core.models import BaseModel
+from projects.models import Project
+from accounts.models import CustomUser
+
+class Board(BaseModel):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='boards')
+    name = models.CharField(_('name'), max_length=255)
+    is_default = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = _('Board')
+        verbose_name_plural = _('Boards')
