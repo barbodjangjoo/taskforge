048f2589ba5504f5b65cbb91a2b38748961a30f4
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-23 10:56:18 +0330

    implement customuser admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'full_name', 'username', 'is_staff', 'is_active', 'date_joined')
+    search_fields = ('email', 'username', 'first_name', 'last_name', 'full_name')
+    ordering = ('-date_joined',)
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('extra information', {'fields': ('avatar', 'bio', 'is_online', 'last_activity')}),
+    )
