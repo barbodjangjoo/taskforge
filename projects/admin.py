0b789b14793255396cc2ed9f46689ee96d6420a9
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-23 18:50:43 +0330

    implement project admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Project, ProjectMember
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'datetime_created', 'is_active')
+    list_filter = ('is_active', 'datetime_created')
+    search_fields = ('name', 'description')
+    # filter_horizontal = ('members',)
+
+
+@admin.register(ProjectMember)
+class ProjectMemberAdmin(admin.ModelAdmin):
+    list_display = ('project', 'user', 'role', 'datetime_created')
+    list_filter = ('role',)
+    raw_id_fields = ('project', 'user')
