85f1c6eefd95b85b3364b6aad3e07fb91c00f4be
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-23 10:53:56 +0330

    implement customUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    
+    email = models.EmailField(_('email address'), unique=True)
+    
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+
+    full_name = models.CharField(_('full name'), max_length=300, )
+    
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    is_online = models.BooleanField(default=False)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+        def __str__(self):
+            return self.email
+        
+        def save(self, *args, **kwargs):
+            if self.first_name or self.last_name:
+                self.full_name = f"{self.first_name} {self.last_name}".strip()
+            super().save(*args, **kwargs)
