2165edb56971a27b829a560df78b1785853f5a1f
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-24 13:10:25 +0330

    implement project viewset

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.db import models
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-# Create your views here.
+from .models import Project
+from .serializers import ProjectSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(
+            models.Q(owner=self.request.user) | 
+            models.Q(members__user=self.request.user)
+        ).distinct()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            owner=self.request.user,
+            created_by=self.request.user
+        )
+
+    @action(detail=True, methods=['post'])
+    def add_member(self, request, pk=None):
+        project = self.get_object()
+        return Response({"message": "عضو اضافه شد"}, status=status.HTTP_200_OK)
