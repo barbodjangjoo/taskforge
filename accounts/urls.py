a38cb696f42d1f30a67e473da6ee808616c63ba4
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-24 15:27:20 +0330

    account urls bugfixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, CustomTokenObtainPairView
+from .views import RegisterView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
