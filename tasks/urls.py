b3c0ac7a7578773cbff4b5a40e3bfe63ee44df91
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-24 18:37:51 +0330

    implement urls api

---
+++ tasks/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BoardViewSet, ColumnViewSet, TaskViewSet
+
+router = DefaultRouter()
+router.register(r'boards', BoardViewSet, basename='board')
+router.register(r'columns', ColumnViewSet, basename='column')
+router.register(r'tasks', TaskViewSet, basename='task')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
