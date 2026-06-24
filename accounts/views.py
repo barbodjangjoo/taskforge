8776288f5b40930edbea534e205fa88953ee57e6
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-24 12:39:45 +0330

    implement user register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+from .serializers import UserSerializer, CustomTokenObtainPairSerializer
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "message": "کاربر با موفقیت ساخته شد",
+            "user": UserSerializer(user).data
+        }, status=status.HTTP_201_CREATED)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
