f16afea1dcc19404d01272b56909a623a54e04f7
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-24 18:37:42 +0330

    implement tasks view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.db import models
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-# Create your views here.
+from .models import Board, Column, Task
+from .serializers import BoardSerializer, ColumnSerializer, TaskSerializer
+
+
+class BoardViewSet(viewsets.ModelViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Board.objects.filter(project__owner=self.request.user) | \
+               Board.objects.filter(project__members__user=self.request.user)
+
+
+class ColumnViewSet(viewsets.ModelViewSet):
+    queryset = Column.objects.all()
+    serializer_class = ColumnSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Column.objects.filter(board__project__owner=self.request.user) | \
+               Column.objects.filter(board__project__members__user=self.request.user)
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(
+            models.Q(project__owner=self.request.user) |
+            models.Q(project__members__user=self.request.user)
+        ).distinct()
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
