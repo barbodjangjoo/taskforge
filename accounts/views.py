797e24303a09ce895d24e67be9011cc454a6f1e3
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-24 15:24:05 +0330

    permission bugfixed

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
 
 
@@ -13,6 +13,7 @@ User = get_user_model()
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
