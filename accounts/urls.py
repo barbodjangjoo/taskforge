d956a70b51078396e56c107fdbd7a618e55f5888
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-27 11:02:25 +0330

    add login to urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from .views import RegisterView
+from . import htmx_views
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('login/', htmx_views.LoginView.as_view(), name='login'),
 ]
