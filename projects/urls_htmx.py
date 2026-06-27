d2063ed827c2a3a73df654f5d71f23a647125b7a
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-06-27 11:02:11 +0330

    bugfixed

--- projects/urls_htmx.py
+++ projects/urls_htmx.py
@@ -6,5 +6,9 @@ app_name = 'projects_htmx'
 urlpatterns = [
     path('', htmx_views.ProjectListView.as_view(), name='home'),
     path('projects/<int:pk>/board/', htmx_views.ProjectBoardView.as_view(), name='project_board'),
+    
+    path('projects/<int:project_id>/add-column-form/', htmx_views.AddColumnFormView.as_view(), name='add_column_form'),
+    path('projects/<int:project_id>/add-task-form/', htmx_views.AddTaskFormView.as_view(), name='add_task_form'),
+    
     path('projects/<int:project_id>/add-column/', htmx_views.CreateColumnView.as_view(), name='add_column'),
 ]
